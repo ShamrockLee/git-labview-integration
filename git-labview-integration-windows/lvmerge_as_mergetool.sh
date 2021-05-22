24e28a65436203c6fcb3a7eb302367442bba6046
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-05-22 14:52:34 -0700

    {windows,posix}/lvmerge_as_mergetool.sh: Fix quotation marks
    * Fix missing quotation mark happen when refactoring.
    
    * Add debug messages

--- git-labview-integration-windows/lvmerge_as_mergetool.sh
+++ git-labview-integration-windows/lvmerge_as_mergetool.sh
@@ -8,10 +8,20 @@ CONFIGURATION_PATH_GIVEN="$1"
 
 source "$SCRIPT_DIR/config_process_header.sh"
 
+echo "BASE: \"$2\"" >&2
+echo "REMOTE: \"$4\"" >&2
+echo "LOCAL: \"$3\"" >&2
+echo "MERGED: \"$5\"" >&2
+
 BASE_FIXED="$(fix_path_configured "$2")"
 REMOTE_FIXED="$(fix_path_configured "$4")"
-LOCAL_FIXED="$(fix_path_configured $3)"
-MERGED_FIXED="$(fix_path_configured $5)"
+LOCAL_FIXED="$(fix_path_configured "$3")"
+MERGED_FIXED="$(fix_path_configured "$5")"
+
+echo "BASE_FIXED: \"$BASE_FIXED\"" >&2
+echo "REMOTE_FIXED: \"$REMOTE_FIXED\"" >&2
+echo "LOCAL_FIXED: \"$LOCAL_FIXED\"" >&2
+echo "MERGED_FIXED: \"$MERGED_FIXED\"" >&2
 
 "$(posixize_windows_path "$LVMERGE_EXECUTABLE")" "$LABVIEW_EXECUTABLE" "$BASE_FIXED" "$REMOTE_FIXED" "$LOCAL_FIXED" "$MERGED_FIXED"
 
